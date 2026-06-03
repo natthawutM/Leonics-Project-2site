923b560eaf98a105694da52f906428e7de8d50e2
Natthawut <rodtang12@gmail.com>
2026-06-03 12:01:22 +0700

    refactor: improve MySQL compatibility functions by ensuring proper function existence checks and enhancing error handling

--- Terusan_MYS/f1/Includes/mysql_compat.php
+++ Terusan_MYS/f1/Includes/mysql_compat.php
@@ -1,120 +1,120 @@
 <?php
-if (function_exists('mysql_connect')) {
-    return;
-}
-
-if (function_exists('mysqli_report')) {
-    mysqli_report(MYSQLI_REPORT_OFF);
-}
-
-if (!defined('MYSQL_ASSOC')) {
-    define('MYSQL_ASSOC', MYSQLI_ASSOC);
-}
-if (!defined('MYSQL_NUM')) {
-    define('MYSQL_NUM', MYSQLI_NUM);
-}
-if (!defined('MYSQL_BOTH')) {
-    define('MYSQL_BOTH', MYSQLI_BOTH);
-}
-
-$GLOBALS['mysql_compat_last_link'] = isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null;
-$GLOBALS['mysql_compat_last_error'] = isset($GLOBALS['mysql_compat_last_error']) ? $GLOBALS['mysql_compat_last_error'] : '';
-
-function mysql_connect($server = null, $username = null, $password = null)
-{
-    try {
-        $link = @mysqli_connect($server, $username, $password);
-    } catch (Exception $e) {
-        $GLOBALS['mysql_compat_last_error'] = $e->getMessage();
-        return false;
+if (!function_exists('mysql_connect')) {
+    if (function_exists('mysqli_report')) {
+        mysqli_report(MYSQLI_REPORT_OFF);
     }
 
-    if ($link) {
-        $GLOBALS['mysql_compat_last_link'] = $link;
-        $GLOBALS['mysql_compat_last_error'] = '';
-    } else {
-        $GLOBALS['mysql_compat_last_error'] = mysqli_connect_error();
+    if (!defined('MYSQL_ASSOC')) {
+        define('MYSQL_ASSOC', MYSQLI_ASSOC);
+    }
+    if (!defined('MYSQL_NUM')) {
+        define('MYSQL_NUM', MYSQLI_NUM);
+    }
+    if (!defined('MYSQL_BOTH')) {
+        define('MYSQL_BOTH', MYSQLI_BOTH);
     }
 
-    return $link;
-}
+    $GLOBALS['mysql_compat_last_link'] = isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null;
+    $GLOBALS['mysql_compat_last_error'] = isset($GLOBALS['mysql_compat_last_error']) ? $GLOBALS['mysql_compat_last_error'] : '';
 
-function mysql_select_db($database_name, $link_identifier = null)
-{
-    $link = $link_identifier ? $link_identifier : (isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null);
-    if (!$link) {
-        $GLOBALS['mysql_compat_last_error'] = 'No active MySQL connection';
-        return false;
+    function mysql_connect($server = null, $username = null, $password = null)
+    {
+        try {
+            $link = @mysqli_connect($server, $username, $password);
+        } catch (Exception $e) {
+            $GLOBALS['mysql_compat_last_error'] = $e->getMessage();
+            return false;
+        }
+
+        if ($link) {
+            $GLOBALS['mysql_compat_last_link'] = $link;
+            $GLOBALS['mysql_compat_last_error'] = '';
+        } else {
+            $GLOBALS['mysql_compat_last_error'] = mysqli_connect_error();
+        }
+
+        return $link;
     }
 
-    try {
-        $ok = @mysqli_select_db($link, $database_name);
-        $GLOBALS['mysql_compat_last_error'] = $ok ? '' : mysqli_error($link);
-    } catch (Exception $e) {
-        $GLOBALS['mysql_compat_last_error'] = $e->getMessage();
-        return false;
-    }
-    return $ok;
-}
+    function mysql_select_db($database_name, $link_identifier = null)
+    {
+        $link = $link_identifier ? $link_identifier : (isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null);
+        if (!$link) {
+            $GLOBALS['mysql_compat_last_error'] = 'No active MySQL connection';
+            return false;
+        }
 
-function mysql_query($query, $link_identifier = null)
-{
-    $link = $link_identifier ? $link_identifier : (isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null);
-    if (!$link) {
-        $GLOBALS['mysql_compat_last_error'] = 'No active MySQL connection';
-        return false;
-    }
+        try {
+            $ok = @mysqli_select_db($link, $database_name);
+            $GLOBALS['mysql_compat_last_error'] = $ok ? '' : mysqli_error($link);
+        } catch (Exception $e) {
+            $GLOBALS['mysql_compat_last_error'] = $e->getMessage();
+            return false;
+        }
 
-    try {
-        $result = @mysqli_query($link, $query);
-        $GLOBALS['mysql_compat_last_error'] = $result === false ? mysqli_error($link) : '';
-    } catch (Exception $e) {
-        $GLOBALS['mysql_compat_last_error'] = $e->getMessage();
-        return false;
-    }
-    return $result;
-}
-
-function mysql_fetch_array($result, $result_type = null)
-{
-    if ($result_type === null) {
-        $result_type = MYSQLI_BOTH;
+        return $ok;
     }
 
-    return mysqli_fetch_array($result, $result_type);
-}
+    function mysql_query($query, $link_identifier = null)
+    {
+        $link = $link_identifier ? $link_identifier : (isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null);
+        if (!$link) {
+            $GLOBALS['mysql_compat_last_error'] = 'No active MySQL connection';
+            return false;
+        }
 
-function mysql_fetch_row($result)
-{
-    return mysqli_fetch_row($result);
-}
+        try {
+            $result = @mysqli_query($link, $query);
+            $GLOBALS['mysql_compat_last_error'] = $result === false ? mysqli_error($link) : '';
+        } catch (Exception $e) {
+            $GLOBALS['mysql_compat_last_error'] = $e->getMessage();
+            return false;
+        }
 
-function mysql_num_rows($result)
-{
-    return mysqli_num_rows($result);
-}
-
-function mysql_close($link_identifier = null)
-{
-    $link = $link_identifier ? $link_identifier : (isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null);
-    if (!$link) {
-        return false;
+        return $result;
     }
 
-    if ((isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null) === $link) {
-        $GLOBALS['mysql_compat_last_link'] = null;
+    function mysql_fetch_array($result, $result_type = null)
+    {
+        if ($result_type === null) {
+            $result_type = MYSQLI_BOTH;
+        }
+
+        return mysqli_fetch_array($result, $result_type);
     }
 
-    return mysqli_close($link);
-}
-
-function mysql_error($link_identifier = null)
-{
-    $link = $link_identifier ? $link_identifier : (isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null);
-    if ($link) {
-        return mysqli_error($link);
+    function mysql_fetch_row($result)
+    {
+        return mysqli_fetch_row($result);
     }
 
-    return isset($GLOBALS['mysql_compat_last_error']) ? $GLOBALS['mysql_compat_last_error'] : mysqli_connect_error();
+    function mysql_num_rows($result)
+    {
+        return mysqli_num_rows($result);
+    }
+
+    function mysql_close($link_identifier = null)
+    {
+        $link = $link_identifier ? $link_identifier : (isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null);
+        if (!$link) {
+            return false;
+        }
+
+        if ((isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null) === $link) {
+            $GLOBALS['mysql_compat_last_link'] = null;
+        }
+
+        return mysqli_close($link);
+    }
+
+    function mysql_error($link_identifier = null)
+    {
+        $link = $link_identifier ? $link_identifier : (isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null);
+        if ($link) {
+            return mysqli_error($link);
+        }
+
+        return isset($GLOBALS['mysql_compat_last_error']) ? $GLOBALS['mysql_compat_last_error'] : mysqli_connect_error();
+    }
 }
 ?>
--- Tetabuan_MYS/f1/Includes/mysql_compat.php
+++ Tetabuan_MYS/f1/Includes/mysql_compat.php
@@ -1,117 +1,120 @@
 <?php
-if (function_exists('mysql_connect')) {
-    return;
-}
-
-if (function_exists('mysqli_report')) {
-    mysqli_report(MYSQLI_REPORT_OFF);
-}
-
-if (!defined('MYSQL_ASSOC')) {
-    define('MYSQL_ASSOC', MYSQLI_ASSOC);
-}
-if (!defined('MYSQL_NUM')) {
-    define('MYSQL_NUM', MYSQLI_NUM);
-}
-if (!defined('MYSQL_BOTH')) {
-    define('MYSQL_BOTH', MYSQLI_BOTH);
-}
-
-$GLOBALS['mysql_compat_last_link'] = isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null;
-$GLOBALS['mysql_compat_last_error'] = isset($GLOBALS['mysql_compat_last_error']) ? $GLOBALS['mysql_compat_last_error'] : '';
-
-function mysql_connect($server = null, $username = null, $password = null)
-{
-    try {
-        $link = @mysqli_connect($server, $username, $password);
-    } catch (Exception $e) {
-        $GLOBALS['mysql_compat_last_error'] = $e->getMessage();
-        return false;
+if (!function_exists('mysql_connect')) {
+    if (function_exists('mysqli_report')) {
+        mysqli_report(MYSQLI_REPORT_OFF);
     }
 
-    if ($link) {
-        $GLOBALS['mysql_compat_last_link'] = $link;
-        $GLOBALS['mysql_compat_last_error'] = '';
-    } else {
-        $GLOBALS['mysql_compat_last_error'] = mysqli_connect_error();
+    if (!defined('MYSQL_ASSOC')) {
+        define('MYSQL_ASSOC', MYSQLI_ASSOC);
     }
-    return $link;
+    if (!defined('MYSQL_NUM')) {
+        define('MYSQL_NUM', MYSQLI_NUM);
+    }
+    if (!defined('MYSQL_BOTH')) {
+        define('MYSQL_BOTH', MYSQLI_BOTH);
+    }
+
+    $GLOBALS['mysql_compat_last_link'] = isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null;
+    $GLOBALS['mysql_compat_last_error'] = isset($GLOBALS['mysql_compat_last_error']) ? $GLOBALS['mysql_compat_last_error'] : '';
+
+    function mysql_connect($server = null, $username = null, $password = null)
+    {
+        try {
+            $link = @mysqli_connect($server, $username, $password);
+        } catch (Exception $e) {
+            $GLOBALS['mysql_compat_last_error'] = $e->getMessage();
+            return false;
+        }
+
+        if ($link) {
+            $GLOBALS['mysql_compat_last_link'] = $link;
+            $GLOBALS['mysql_compat_last_error'] = '';
+        } else {
+            $GLOBALS['mysql_compat_last_error'] = mysqli_connect_error();
+        }
+
+        return $link;
+    }
+
+    function mysql_select_db($database_name, $link_identifier = null)
+    {
+        $link = $link_identifier ? $link_identifier : (isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null);
+        if (!$link) {
+            $GLOBALS['mysql_compat_last_error'] = 'No active MySQL connection';
+            return false;
+        }
+
+        try {
+            $ok = @mysqli_select_db($link, $database_name);
+            $GLOBALS['mysql_compat_last_error'] = $ok ? '' : mysqli_error($link);
+        } catch (Exception $e) {
+            $GLOBALS['mysql_compat_last_error'] = $e->getMessage();
+            return false;
+        }
+
+        return $ok;
+    }
+
+    function mysql_query($query, $link_identifier = null)
+    {
+        $link = $link_identifier ? $link_identifier : (isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null);
+        if (!$link) {
+            $GLOBALS['mysql_compat_last_error'] = 'No active MySQL connection';
+            return false;
+        }
+
+        try {
+            $result = @mysqli_query($link, $query);
+            $GLOBALS['mysql_compat_last_error'] = $result === false ? mysqli_error($link) : '';
+        } catch (Exception $e) {
+            $GLOBALS['mysql_compat_last_error'] = $e->getMessage();
+            return false;
+        }
+
+        return $result;
+    }
+
+    function mysql_fetch_array($result, $result_type = null)
+    {
+        if ($result_type === null) {
+            $result_type = MYSQLI_BOTH;
+        }
+
+        return mysqli_fetch_array($result, $result_type);
+    }
+
+    function mysql_fetch_row($result)
+    {
+        return mysqli_fetch_row($result);
+    }
+
+    function mysql_num_rows($result)
+    {
+        return mysqli_num_rows($result);
+    }
+
+    function mysql_close($link_identifier = null)
+    {
+        $link = $link_identifier ? $link_identifier : (isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null);
+        if (!$link) {
+            return false;
+        }
+
+        if ((isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null) === $link) {
+            $GLOBALS['mysql_compat_last_link'] = null;
+        }
+
+        return mysqli_close($link);
+    }
+
+    function mysql_error($link_identifier = null)
+    {
+        $link = $link_identifier ? $link_identifier : (isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null);
+        if ($link) {
+            return mysqli_error($link);
+        }
+
+        return isset($GLOBALS['mysql_compat_last_error']) ? $GLOBALS['mysql_compat_last_error'] : mysqli_connect_error();
+    }
 }
-
-function mysql_select_db($database_name, $link_identifier = null)
-{
-    $link = $link_identifier ? $link_identifier : (isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null);
-    if (!$link) {
-        $GLOBALS['mysql_compat_last_error'] = 'No active MySQL connection';
-        return false;
-    }
-
-    try {
-        $ok = @mysqli_select_db($link, $database_name);
-        $GLOBALS['mysql_compat_last_error'] = $ok ? '' : mysqli_error($link);
-    } catch (Exception $e) {
-        $GLOBALS['mysql_compat_last_error'] = $e->getMessage();
-        return false;
-    }
-    return $ok;
-}
-
-function mysql_query($query, $link_identifier = null)
-{
-    $link = $link_identifier ? $link_identifier : (isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null);
-    if (!$link) {
-        $GLOBALS['mysql_compat_last_error'] = 'No active MySQL connection';
-        return false;
-    }
-
-    try {
-        $result = @mysqli_query($link, $query);
-        $GLOBALS['mysql_compat_last_error'] = $result === false ? mysqli_error($link) : '';
-    } catch (Exception $e) {
-        $GLOBALS['mysql_compat_last_error'] = $e->getMessage();
-        return false;
-    }
-    return $result;
-}
-
-function mysql_fetch_array($result, $result_type = null)
-{
-    if ($result_type === null) {
-        $result_type = MYSQLI_BOTH;
-    }
-    return mysqli_fetch_array($result, $result_type);
-}
-
-function mysql_fetch_row($result)
-{
-    return mysqli_fetch_row($result);
-}
-
-function mysql_num_rows($result)
-{
-    return mysqli_num_rows($result);
-}
-
-function mysql_close($link_identifier = null)
-{
-    $link = $link_identifier ? $link_identifier : (isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null);
-    if (!$link) {
-        return false;
-    }
-
-    if ((isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null) === $link) {
-        $GLOBALS['mysql_compat_last_link'] = null;
-    }
-
-    return mysqli_close($link);
-}
-
-function mysql_error($link_identifier = null)
-{
-    $link = $link_identifier ? $link_identifier : (isset($GLOBALS['mysql_compat_last_link']) ? $GLOBALS['mysql_compat_last_link'] : null);
-    if ($link) {
-        return mysqli_error($link);
-    }
-
-    return isset($GLOBALS['mysql_compat_last_error']) ? $GLOBALS['mysql_compat_last_error'] : mysqli_connect_error();
-}
+?>
